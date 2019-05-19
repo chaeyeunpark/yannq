c0265cab3161d1209dd84e05058229bc04d11bcb
cecri <chae.yeun.park@gmail.com>
2019-05-19 14:53:07 +0200

    Hamtilonain test added

---
+++ Tests/TestHamTFI.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+#include "Tests/HamiltonianHelper.hpp"
+#include "Hamiltonians/TFIsing.hpp"
+
+TEST_CASE("Test Hamiltonian TFI", "[TFI]")
+{
+	constexpr int N = 12;
+	constexpr double h = 0.2;
+	TFIsing ham(N, 1.0, h);
+	
+	for(uint32_t col = 0; col < (1<<N); col++)
+	{
+		auto colSt = getColFromStates(ham, col);
+		auto colHam = ham(col);
+
+		REQUIRE(diffMap(colSt, colHam) < 1e-6);
+	}
+}
