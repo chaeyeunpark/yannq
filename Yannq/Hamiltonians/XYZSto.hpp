b6c4b18e866b4c41fb1194bb343bbc52ce520bb4
cecri <chae.yeun.park@gmail.com>
2020-01-26 15:48:59 +0100

    XYZNNNSto fixed

--- Yannq/Hamiltonians/XYZSto.hpp
+++ Yannq/Hamiltonians/XYZSto.hpp
@@ -1,7 +1,9 @@
-#ifndef CY_XYZ_NNN_HPP
-#define CY_XYZ_NNN_HPP
+#ifndef HAMILTONIANS_XYZNNNSTO_HPP
+#define HAMILTONIANS_XYZNNNSTO_HPP
 #include <Eigen/Eigen>
+#include <nlohmann/json.hpp>
 
+/* Stoquastic for |a|, |b| < 1*/
 class XYZNNNSto
 {
 private:
@@ -16,51 +18,50 @@ public:
 		: n_(n), a_(a), b_(b)
 	{
 	}
+
+	nlohmann::json params() const
+	{
+		return nlohmann::json
+		{
+			{"name", "XYZNNNSto"},
+			{"n", n_},
+			{"a", a_},
+			{"b", b_}
+		};
+	}
 	
 	template<class State>
-	typename State::ValueType operator()(const State& smp) const
+	typename State::T operator()(const State& smp) const
 	{
-		typename State::ValueType s = 0.0;
+		typename State::T s = 0.0;
 		//Nearest-neighbor
 		for(int i = 0; i < n_; i++)
 		{
 			double yysign = -smp.sigmaAt(i)*smp.sigmaAt((i+1)%n_);
-			s += -J1*b_*yysign; //zz
-			s += J1*(a_+yysign*1.0)*smp.ratio(i, (i+1)%n_); //xx+yy
+			s += -J1*a_*yysign; //zz
+			s += J1*(1.0+b_*yysign)*smp.ratio(i, (i+1)%n_); //xx+yy
 		}
 		//Next-nearest-neighbor
 		for(int i = 0; i < n_; i++)
 		{
 			double yysign = -smp.sigmaAt(i)*smp.sigmaAt((i+2)%n_);
-			s += -J2*a_*yysign; //zz
-			s += J2*(b_+yysign*1.0)*smp.ratio(i, (i+2)%n_); //xx+yy
+			s += -J2*b_*yysign; //zz
+			s += J2*(1.0+a_*yysign)*smp.ratio(i, (i+2)%n_); //xx+yy
 		}
 		return s;
 	}
-
-	std::vector< std::array<int, 2> > flips() const
-	{
-		std::vector< std::array<int, 2> > res;
-		for(int i = 0; i < n_; i++)
-		{
-			res.push_back(std::array<int, 2>{i,(i+1)%n_});
-			res.push_back(std::array<int, 2>{i,(i+2)%n_});
-		}
-		return res;
-	}
 	
-	/*
-	Eigen::VectorXd getCol(long long int col) const
+	std::map<uint32_t, double> operator()(uint32_t col) const
 	{
-		Eigen::VectorXd res = Eigen::VectorXd::Zero(1<<n_);
+		std::map<uint32_t, double> m;
 		for(int i = 0; i < n_; i++)
 		{
 			int b1 = (col >> i) & 1;
 			int b2 = (col >> ((i+1)%n_)) & 1;
 			int sgn = (1-2*b1)*(1-2*b2);
 			long long int x = (1 << i) | (1 << ((i+1)%(n_)));
-			res(col ^ x) += J1*(a_ - sgn*b_);
-			res(col) += J1*sgn;
+			m[col ^ x] += J1*(1.0 - sgn*b_);
+			m[col] += J1*a_*sgn;
 		}
 		for(int i = 0; i < n_; i++)
 		{
@@ -68,11 +69,10 @@ public:
 			int b2 = (col >> ((i+2)%n_)) & 1;
 			int sgn = (1-2*b1)*(1-2*b2);
 			long long int x = (1 << i) | (1 << ((i+2)%(n_)));
-			res(col ^ x) += J2*(b_ - sgn*a_);
-			res(col) += J2*sgn;
+			m[col ^ x] += J2*(1.0 - sgn*a_);
+			m[col] += J2*b_*sgn;
 		}
-		return res;
+		return m;
 	}
-	*/
 };
-#endif//CY_XYZ_NNN_HPP
+#endif//HAMILTONIANS_XYZNNNSTO_HPP
